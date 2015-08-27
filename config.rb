4d6504a9196c672c37488e2845f6e3ee4e74c99b
luiz felipe <luizfelipejr@outlook.com>
2015-08-27 12:02:54 -0300

    create initial html and sass structure

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@ require 'bootstrap-sass'
 # require "normalize-scss"
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
+http_path = "assets/"
+css_dir = "assets/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed # :compressed # :expanded or :compact or :nested
